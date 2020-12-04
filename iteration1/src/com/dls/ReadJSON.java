1feefd55ed6152f97857c1d11d21ce34e5e667b1
Ahmet Harun Tokyer <74705538+a-haruntokyer@users.noreply.github.com>
2020-12-04 20:36:53 +0300

    Config Read Added

--- iteration1/src/com/dls/ReadJSON.java
+++ iteration1/src/com/dls/ReadJSON.java
@@ -15,11 +15,12 @@ public class ReadJSON {
     private Dataset dataset;
     private String jsonPath;
 
-    public ReadJSON(Dataset dataset, String jsonPath){
+    public ReadJSON(String jsonPath){
 
-        setDataset(dataset);
+        //setDataset(dataset);
         setJsonPath(jsonPath);
-        read();
+        //read();
+        readConfig();
     }
 
     protected void read() {
@@ -95,5 +96,74 @@ public class ReadJSON {
 
     }
 
+    protected void readInput(){
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object jsonParser = parser.parse(new FileReader(jsonPath));
+            JSONObject jsonObject = (JSONObject) jsonParser;
+
+            JSONArray instanceList = (JSONArray) jsonObject.get("instances");
+            JSONArray labelList = (JSONArray) jsonObject.get("class labels");
+            int datasetId =Integer.parseInt(jsonObject.get("dataset id").toString());
+            String datasetName =(String) jsonObject.get("dataset name");
+            int maxnumberoflabels =Integer.parseInt(jsonObject.get("maximum number of labels per instance").toString());
+            Dataset dataset = new Dataset(datasetId, datasetName, maxnumberoflabels);
+            setDataset(dataset);
+            parseInstanceObject(instanceList);
+            parseLabelObject(labelList);
+            WriteJSON wj = new WriteJSON(dataset);
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+    protected void readConfig(){
+        JSONParser parser = new JSONParser();
+        try {
+            Object jsonParser = parser.parse(new FileReader(jsonPath));
+            JSONObject jsonObject = (JSONObject) jsonParser;
+
+            JSONArray userList = (JSONArray) jsonObject.get("users");
+            String inputPath = (String) jsonObject.get("input path");
+            String outPath = (String) jsonObject.get("output path");
+            System.out.println(inputPath);
+            setJsonPath(inputPath);
+            readInput();
+            parseUsers(userList);
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+    protected void parseUsers(JSONArray userList) {
+
+        int size = userList.size();
+
+        for (int i = 0; i < size; i++) {
+
+            JSONObject usersObject = (JSONObject) userList.get(i);
+
+            int UserId = Integer.parseInt(usersObject.get("user id").toString());
+            String userName = (String) usersObject.get("user name");
+            String userType = (String) usersObject.get("user type");
+            String passwords = (String) usersObject.get("user name");
+            System.out.println(userName);
+            // dataset add user
+            //this.dataset.addInstance(instanceId, instanceText);
+
+        }
+
+    }
 
 }
