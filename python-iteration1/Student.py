43d055c314d6701a88168cd68b6baca9671525a5
Ahmet Harun Tokyer <74705538+a-haruntokyer@users.noreply.github.com>
2021-01-08 01:39:57 +0300

    Python Iteration1

---
+++ python-iteration1/Student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,name,surname,mail):
+        self.name = name
+        self.surname = surname
+        self.mail = mail
+
+    def func(student):
+        print("Student name is " + student.name)
