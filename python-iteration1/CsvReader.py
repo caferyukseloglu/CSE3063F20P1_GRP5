3ac7c0c116a86f94b0000ca92eaaad770f504dfb
orhunozkaya <74686017+orhunozkaya@users.noreply.github.com>
2021-01-08 21:30:10 +0300

    CsvReader Updated 1

--- python-iteration1/CsvReader.py
+++ python-iteration1/CsvReader.py
@@ -1,37 +1,42 @@
 import csv
 import xlrd
-from Student import Student
-import Questions
+import os,sys
 class CsvReader:
-
-    def readpoll(self):
-
-        users={}
-        students=[]
-        with open('CSE3063_20201124_Tue_zoom_PollReport.csv', mode="r",encoding="utf8") as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter =",")
-            list=[]
-            for row in csv_reader:
-                list.append(row)
-
-
-
-
-            for i in range(1,len(list)):
-                split_row = str(list[i][1]).split(" ")
-                for a in range(4,(len(list[i])-1),2):
-                    questions = {}
-                    questions[list[i][a]]= list[i][a+1]
-                    users[(list[i][1])] = questions
-
-                #print(users[(list[i][1])])
-
-                student =Student(split_row[0].upper(),split_row[-1].upper(),list[i][2])
-                #student.func()
-
-            #print(questions)
-            for key in users.keys():
-                print(users[key])
-
-csvread= CsvReader()
-csvread.readpoll()
+    def __init__(self,file):
+        self.file=file
+    def bys_reader(self):
+        try:
+            read_xls=xlrd.open_workbook(self.file)
+            xl_sheet = read_xls.sheet_by_index(0) #In order to read specific sheet such as rptSinifListesi we'll use this method.
+            print(f'Sheet name: {xl_sheet.name}')
+            col_of_name = xl_sheet.col_values(4)
+            col_of_surname=xl_sheet.col_values(7)
+            for i in range(min(len(col_of_name),len(col_of_surname))):
+                if col_of_name[i]=="" or col_of_name[i]=="Adı" or col_of_surname[i]=="" or col_of_surname[i]=="Soyadı":
+                    pass
+                else:
+                    print(col_of_name[i], col_of_surname[i])
+        except Exception as err:
+            print("Error Occured",err)
+    def poll_report_reader(self):
+        print("Poll Name: {}".format(os.path.basename(self.file)))
+        with open(self.file, newline='',encoding="utf8") as csvfile:
+            csv_array = csv.reader(csvfile)
+            i = 0
+            for row in csv_array:
+                if i == 0:
+                    pass
+                else:
+                    name = row[1]
+                    email = row[2]
+                        # add_student( fullname, email )
+                    for j in range(4, len(row) - 1, 2):
+                        question = row[j]
+                        answer = row[j + 1]
+                            # add_question ( question, email )
+                        print(question,answer)
+                i = i + 1
+#            elif self.csv_file2.lower().endswith(".xls"):
+#                pass
+data=CsvReader("CSE3063_20201124_Tue_zoom_PollReport.csv")
+data.poll_report_reader()
