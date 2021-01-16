f9dffa32e60017591b30e4527247c0c1b202e1f3
serkanhelvacioglı <serkanhelvacioglu@outlook.com>
2021-01-16 22:18:01 +0300

    Exporter Chart Update

--- python-iteration1/ZoomPollViewer/Exporter.py
+++ python-iteration1/ZoomPollViewer/Exporter.py
@@ -7,6 +7,12 @@ ZOOM POLL VIEWER v0.1
 import xlsxwriter
 from datetime import date
 
+from vincent.colors import brews
+
+question_section={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'}
+# Some sample data to plot.
+cat_4 = [ question_section[x] for x in range(1, 8)]
+
 
 class Exporter():
     def __init__(self,zpv):
@@ -67,11 +73,27 @@ class Exporter():
                     else:
                         c += 3
         self.close_xlsx()                
+    def draw_histogram(self,type,column):
+        self.create_xlsx('Global.xlsx')
+        self.create_xlsx_page('Histogram')
+        chart=self.xlsx_file.add_chart({type:column})
+        for col_num in range(1, len(cat_4) + 1):
+            chart.add_series({
+                'name': ['Sheet1', 0, col_num],
+                'categories': ['Sheet1', 1, 0, len(cat_4) + 1, 0],
+                'values': ['Sheet1', 1, col_num, len(cat_4) + 1, col_num],
+                'fill': {'color': brews['Spectral'][col_num - 1]},
+                'gap': 300,
+            })
 
-#m = Exporter()
-# m.create_xlsx('Poll.xlsx')
-# m.create_xlsx_page('Pager-1')
-# m.write_xlsx_page(0,0,'Selam')
-# m.create_xlsx_page('Pager-2')
-# m.write_xlsx_page(0,0,'Selam2')
-#m.close_xlsx()
+        chart.set_y_axis({'major_gridlines':{'visible':False}})
+        self.xlsxpage.insert_chart('K2',chart)
+
+
+m = Exporter()
+m.create_xlsx('Poll.xlsx')
+m.create_xlsx_page('Pager-1')
+m.write_xlsx_page(0,0,'Selam')
+m.create_xlsx_page('Pager-2')
+m.write_xlsx_page(0,0,'Selam2')
+m.close_xlsx()
