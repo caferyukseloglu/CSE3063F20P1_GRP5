ec26629e09f0fe2ad0318f54be65ffd5f627deac
Emin Safa Tok <esafa.tok@gmail.com>
2021-01-25 17:23:53 +0300

    Directory created

--- python-iteration1/Questions.py
+++
@@ -1,5 +0,0 @@
-class Questions:
-    def __init__(self,text,answer):
-        self.text = text
-        self.answer = answer
-
--- python-iteration1/Student.py
+++
@@ -1,8 +0,0 @@
-class Student:
-    def __init__(self,name,surname,mail):
-        self.name = name
-        self.surname = surname
-        self.mail = mail
-
-    def func(student):
-        print("Student name is " + student.name)
